02ea62eb7ad903cc706f4548a8b245958fcbe6f7
Natasha <njohnson533@gmail.com>
2023-02-25 19:53:51 -0500

    update index.js file

--- public/admin/index.js
+++ public/admin/index.js
@@ -1,6 +1,36 @@
-import Index from '../data/index'
-// register your collection(s) here
-CMS.registerCollection(Index);
+import CMS from "netlify-cms-app";
+import Index from "../public/data/index.yml";
 
-// start the CMS
+// Register the collection for the Index data
+CMS.registerPreviewStyle("/admin/cms.css");
+CMS.registerCollection({
+  name: "index",
+  label: "Index",
+  files: [
+    {
+      label: "Index",
+      name: "index",
+      file: "../public/data/index.yml",
+      fields: [
+        {
+          label: "Heading",
+          name: "heading",
+          widget: "string",
+        },
+        {
+          label: "Subheading",
+          name: "subheading",
+          widget: "markdown",
+        },
+        {
+          label: "Background Image",
+          name: "background_image",
+          widget: "image",
+        },
+      ],
+    },
+  ],
+});
+
+// Initialize the CMS
 CMS.init();
